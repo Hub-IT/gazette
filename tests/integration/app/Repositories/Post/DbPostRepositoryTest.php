4e6edfb3b603303aa9a1cab6284ead7cfd820743
Rizart Dokollari <r.dokollari@gmail.com>
2015-09-13 02:52:26 +0000

    Hotfix: Correctly return latest posts, including their categories & authors.

--- tests/integration/app/Repositories/Post/DbPostRepositoryTest.php
+++ tests/integration/app/Repositories/Post/DbPostRepositoryTest.php
@@ -31,8 +31,16 @@ class DbPostRepositoryTest extends \TestCase
 
 		$this->assertCount(2, $actualLatestPosts);
 		$this->assertCount(3, $this->dbPostRepository->all());
-		$this->assertEquals($expectedLatestPosts[0], $actualLatestPosts->get(0));
+
+		$this->assertEquals($expectedLatestPosts[0]->id, $actualLatestPosts->get(0)->id);
+		$this->assertEquals($expectedLatestPosts[0]->title, $actualLatestPosts->get(0)->title);
+		$this->assertEquals($expectedLatestPosts[0]->author, $actualLatestPosts->get(0)->author);
+		$this->assertEquals($expectedLatestPosts[0]->category, $actualLatestPosts->get(0)->category);
+
+		$this->assertEquals($expectedLatestPosts[1]->id, $actualLatestPosts->get(1)->id);
 		$this->assertEquals($expectedLatestPosts[1]->title, $actualLatestPosts->get(1)->title);
+		$this->assertEquals($expectedLatestPosts[1]->author, $actualLatestPosts->get(1)->author);
+		$this->assertEquals($expectedLatestPosts[1]->category, $actualLatestPosts->get(1)->category);
 	}
 
 	/** @test */
