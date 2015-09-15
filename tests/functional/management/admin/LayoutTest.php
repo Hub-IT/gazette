19de61a2a1c4a855201f870f426a129f1336679f
Rizart Dokollari <r.dokollari@gmail.com>
2015-09-15 00:26:52 +0000

    Use lowercase letter on bower.json to comply with bowerphp.

--- tests/functional/management/admin/LayoutTest.php
+++ tests/functional/management/admin/LayoutTest.php
@@ -33,30 +33,4 @@ class LayoutTest extends TestCase
 //			->see('GMS');
 	}
 
-	/** @test */
-	public function it_reads_sidebar()
-	{
-		$this->visit(route('management.dashboard'))
-			->seePageIs(route('management.dashboard'))
-			->see('<span class="menu-trigger animated fadeInDown"')
-			->see('Gazzete')
-			->see('DEREE News. 24/7.')
-			->see('Latest story.')
-			->see('Subtitle')
-			->see('Join Our Newsletter');
-	}
-
-	/** @test */
-	public function it_reads_footer()
-	{
-		$this->visit(route('home'))
-			->see('Home')
-			->see('Contributors')
-			->see('About')
-			->see('Contact')
-			->see('Articles')
-			->see('Categories')
-			->see('Tags')
-			->see('Favorites');
-	}
 }
